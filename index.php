c5d2bff7cf834ef4b1adfdca4a064ead6dc1ed62
matulisd <nekton.dominykas@gmail.com>
2022-12-05 22:56:30 +0200

    Login page front end

--- index.php
+++ index.php
@@ -2,19 +2,31 @@
     include_once 'header.php';
 ?>
 
-<div class="container">
-    <div class="row">
-        <section class="col-6 login login-left">
-            asdf
+<div class="container width90">
+    <div class="row login">
+        <section class="col-md-6 col-sm-12 login-left">
+            <!-- pakeisti i besikeicianti teksta + vaikinas iesko merginos -->
+            <h2 class="login-txt-left">Mergina ieško vaikino</h2>
+            <img class="login-img" src="./img/korteles.png" alt="Naudotojų profilių nuotraukos">
+            <h3 class="login-txt-left2">Ar tai būsi TU? Pažintys mygtuko paspaudimu - 
+                surask savo antrą pusę!
+            </h3>
         </section>
-        <section class="col-6 login login-right">
-            asdf
+        <section class="col-md-6 col-sm-12 login-right">
+            <h4 class="login-txt">Turi paskyrą? Prisijunk!
+            </h4>
+            <form class="login-form">
+            <input type="email" class="form-control login-input" 
+            placeholder="El. pašto adresas">
+            <input type="password" class="form-control login-input"
+            placeholder="Slaptažodis">
+            <input type="submit" class="button-black" value="Prisijungti">
+            </form>
+            <p class="login-txt-small">arba</p>
+            <button class="button-black">Registruotis</button>
         </section>
     </div>
 <div>
-
-<h1>this is index</h1>
-
 
 <?php
     include_once 'footer.php';
